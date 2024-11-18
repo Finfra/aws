2a677a4391ee7b9739eaeaf8bd40f0c38591a0f5
NamJungGu <nowage@gmail.com>
2024-11-18 21:47:11 +0900

    Update variables.tf

--- 1.6.2.RDS/variables.tf
+++ 1.6.2.RDS/variables.tf
@@ -8,7 +8,7 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "ap-northeast-2"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
